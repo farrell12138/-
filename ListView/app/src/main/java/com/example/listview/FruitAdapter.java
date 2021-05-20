8b4d8dda7da817896eca7cac1a9e3882fe5c3dae
fengzijun <fengzijun@meituan.com>
2021-05-20 17:14:48 +0800

    ListView

---
+++ ListView/app/src/main/java/com/example/listview/FruitAdapter.java
@@ -0,0 +1,50 @@
+package com.example.listview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
+import java.util.List;
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+    private int resourceID;
+
+    public FruitAdapter(@NonNull Context context, int textViewResourceId, @NonNull List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+        resourceID = textViewResourceId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        View view;
+        ViewHolder viewHolder = new ViewHolder();
+        if(convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(resourceID, parent, false);
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);
+        }
+        else{
+           view = convertView;
+           viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
+        return view;
+    }
+    class ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
+    }
+}
